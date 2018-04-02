ab8147cc197e407a65b973cc77adbf22170c05de
zlraymind <zlraymind@outlook.com>
2018-04-02 15:05:53 +0800

    Update ios proj.

--- 01/ios/RongSDK_iOS/ViewController.h
+++ 01/ios/RongSDK_iOS/ViewController.h
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
-// 步骤-2，添加融云头文件
+
+// 步骤-2：添加融云 SDK 头文件。
 #import <RongIMLib/RongIMLib.h>
 
+// 步骤-6-1：接受消息，声明代理的使用者。
 @interface ViewController : UIViewController<RCIMClientReceiveMessageDelegate>
-
 
 @end
 
--- 01/ios/RongSDK_iOS/ViewController.m
+++ 01/ios/RongSDK_iOS/ViewController.m
@@ -26,7 +26,42 @@
     // Dispose of any resources that can be recreated.
 }
 
-// 步骤-6，实现代理方法
+- (IBAction)onInitSDK:(id)sender {
+    // 步骤-3：初始化 SDK。appKey 一定要和申请 token 时使用的 appKey 一致。
+    [[RCIMClient sharedRCIMClient] initWithAppKey:@"8brlm7ufrg9e3"];
+}
+
+// 步骤-4：登录。
+- (IBAction)onConnect:(id)sender {
+//    [HttpRequest getToken:@"User_B" tokenHandler:^(NSString *token) {
+        // 通过 Http 向 AppServer 请求 token。
+        NSString *token = @"vRdrw6Mr1e8cPY6vJuU5+x+sHndqNeBH080EFO5XARsVjheEJoaYkoIB08EiA7Amcf0wuO8eciWSCH9hgfur9Q==";
+        [[RCIMClient sharedRCIMClient] connectWithToken:token
+                                                success:^(NSString *userId) {
+                                                    NSLog(@"登陆成功。当前登录的用户ID：%@", userId);
+                                                } error:^(RCConnectErrorCode status) {
+                                                    NSLog(@"登陆的错误码为:%ld", status);
+                                                } tokenIncorrect:^{
+                                                    NSLog(@"token错误");
+                                                }];
+//    }];
+}
+
+// 步骤-5：发送消息。
+- (IBAction)onSendMsg:(id)sender {
+    RCTextMessage *msg = [RCTextMessage messageWithContent:@"Hello User_A"];
+    [[RCIMClient sharedRCIMClient] sendMessage:ConversationType_PRIVATE
+                                      targetId:@"User_A" content:msg
+                                   pushContent:nil pushData:nil
+                                       success:nil error:nil];
+}
+
+- (IBAction)onSetReceived:(id)sender {
+    // 步骤-6-2：接受消息，设置消息监听。
+    [[RCIMClient sharedRCIMClient] setReceiveMessageDelegate:self object:nil];
+}
+
+// 步骤-6-3：接受消息，实现代理方法。
 - (void)onReceived:(RCMessage *)message
               left:(int)nLeft
             object:(id)object {
@@ -37,35 +72,23 @@
     NSLog(@"还剩余的未接收的消息数：%d", nLeft);
 }
 
-- (IBAction)onInit:(id)sender {
-    // 步骤-3，初始化 SDK
-    [[RCIMClient sharedRCIMClient] initWithAppKey:@"8brlm7ufrg9e3"];
-    
-    // 步骤-5，设置消息监听
-    [[RCIMClient sharedRCIMClient] setReceiveMessageDelegate:self object:nil];
+// 步骤-7：获取未读消息数。
+- (IBAction)onGetUnread:(id)sender {
+    int totalUnreadCount = [[RCIMClient sharedRCIMClient] getTotalUnreadCount];
+    NSLog(@"当前所有会话的未读消息数为：%d", totalUnreadCount);
 }
 
-// 步骤-4，登录
-- (IBAction)onConnect:(id)sender {
-    [HttpRequest getToken:@"User_B" tokenHandler:^(NSString *token) {
-        [[RCIMClient sharedRCIMClient] connectWithToken:token
-                                                success:^(NSString *userId) {
-                                                    NSLog(@"登陆成功。当前登录的用户ID：%@", userId);
-                                                } error:^(RCConnectErrorCode status) {
-                                                    NSLog(@"登陆的错误码为:%ld", status);
-                                                } tokenIncorrect:^{
-                                                    NSLog(@"token错误");
-                                                }];
-    }];
-}
-
-// 步骤-7，发送消息
-- (IBAction)onSend:(id)sender {
-    RCTextMessage *msg = [RCTextMessage messageWithContent:@"Hello User_A"];
-    [[RCIMClient sharedRCIMClient] sendMessage:ConversationType_PRIVATE
-                                      targetId:@"User_A" content:msg
-                                   pushContent:nil pushData:nil
-                                       success:nil error:nil];
+- (IBAction)onGetConverList:(id)sender {
+    NSArray *conversationList = [[RCIMClient sharedRCIMClient]
+                                 getConversationList:@[@(ConversationType_PRIVATE),
+                                                       @(ConversationType_DISCUSSION),
+                                                       @(ConversationType_GROUP),
+                                                       @(ConversationType_SYSTEM),
+                                                       @(ConversationType_APPSERVICE),
+                                                       @(ConversationType_PUBLICSERVICE)]];
+    for (RCConversation *conversation in conversationList) {
+        NSLog(@"会话类型：%lu，目标会话ID：%@", (unsigned long)conversation.conversationType, conversation.targetId);
+    }
 }
 
 @end
