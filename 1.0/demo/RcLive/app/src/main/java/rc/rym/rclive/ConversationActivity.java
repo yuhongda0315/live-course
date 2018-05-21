99b40f7ca870c919f1d853a41cba9dd90ccf24a7
zlraymind <zlraymind@outlook.com>
2018-05-21 17:42:16 +0800

    添加 Baymax 问题的解答。

--- 1.0/demo/RcLive/app/src/main/java/rc/rym/rclive/ConversationActivity.java
+++ 1.0/demo/RcLive/app/src/main/java/rc/rym/rclive/ConversationActivity.java
@@ -3,11 +3,24 @@ package rc.rym.rclive;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import io.rong.imkit.fragment.ConversationFragment;
+
 public class ConversationActivity extends AppCompatActivity {
+    private ConversationFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+        // for Baymax.
+        fragment = (ConversationFragment) getSupportFragmentManager().findFragmentById(R.id.conversation_fragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // for Baymax.
+        if (fragment != null && !fragment.onBackPressed()) {
+            super.onBackPressed();
+        }
     }
 }
